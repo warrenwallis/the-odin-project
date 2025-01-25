e854cf4d41a016a9ae624b522b120d13130d3bca
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-01-24 17:08:41 -0800

    Finish Baseline Logic for Score Component
    Made the createElement function into a constructor ~ automatically calling it when the Score component is called
    
    Update display to automatically update score and display instead of having a separate display function

--- tic-tac-toe/header.js
+++ tic-tac-toe/header.js
@@ -1,6 +1,7 @@
 const Score = (parent, id, name, score = 0) => {
+    const playerScore = document.createElement('div');
 
-    const createElement = () => {
+    const constructor = (() => {
         const container = document.createElement('div');
         container.setAttribute('style', 'display: flex; flex-direction: col; background: var(--primary-color-darkest); color: var(--primary-color-lightest); padding: 15px 40px; border-radius: 15px; gap: 5px');
 
@@ -8,35 +9,29 @@ const Score = (parent, id, name, score = 0) => {
         container.textContent = `${name}:`;
         container.appendChild(playerHandle);
         
-        const playerScore = document.createElement('div');
         playerScore.textContent = `${score}`;
         container.appendChild(playerScore);
         
         parent.appendChild(container);
+    })();
+
+    const updateScore = () => {
+        playerScore.textContent = `${++score}`;
     }
 
-    const display = () => {
-        score++;
-
-        const currScore = document.getElementById(`${id}`);
-
-    }
-
-    return { id, createElement };
+    return { id, updateScore };
 }
 
 const header = document.getElementById('header');
 header.setAttribute('style', 'display: flex; background: var(--primary-color-lightest); padding: 25px 120px');
 
 const pageTitle = document.createElement('div');
-pageTitle.setAttribute('style', 'display: flex; align-items: center; flex: 1;');
+pageTitle.setAttribute('style', 'display: flex; align-items: center; flex: 1; color: vars(--primary-color-darkest); font-size: 26px');
 pageTitle.textContent = 'Tic Tac Toe';
 header.appendChild(pageTitle);
 
 const PlayerScoreBoard = document.createElement('div');
 PlayerScoreBoard.setAttribute('style', 'display: flex;  gap: 10px; justify-content: end; flex: 1;');
 const player1 = Score(PlayerScoreBoard, 'player1ScoreBoard', 'Player 1');
-player1.createElement();
 const player2 = Score(PlayerScoreBoard, 'player2ScoreBoard', 'Player 2');
-player2.createElement();
 header.appendChild(PlayerScoreBoard);
