5cc198df9aa5178736dcdf1ef442b6987316d24c
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-01-31 15:37:48 -0800

    Add Menu Items
    Created a json file to host information. As menu items increase, it will be best to host a server and pull info from backend.
    Added photos from Figma.
    Style each menu item appropriately.

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,10 +1,13 @@
 import './styles.css'
 import Home from './Home';
+import Menu from './Menu';
 import NavBarLogic from './NavBar/logic';
 
 const navBarLogic = new NavBarLogic();
 
 const content = document.getElementById('content');
-const home = Home({ parent: content });
+// const home = Home({ parent: content });
+
+const about = Menu({ parent: content });
 
 console.log("working");
