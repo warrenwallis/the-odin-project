7df723e0132cf0774c60ff7d73bdc42a633ee3cb
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-01-31 13:26:17 -0800

    Style and Add Logic to Header
    Be able to click between navigation links and appropriately adding the underline to the current choice.
    Create a class to handle setting and getting the correct navigation IDs, and add event listeners to the specific buttons.

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,5 +1,8 @@
 import './styles.css'
 import Home from './Home';
+import NavBarLogic from './NavBar/logic';
+
+const navBarLogic = new NavBarLogic();
 
 const content = document.getElementById('content');
 const home = Home({ parent: content });
