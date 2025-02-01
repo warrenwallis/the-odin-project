bc3bf203aa20b4ca05284ecd4e9734613e7fb6bd
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-01-31 22:41:40 -0800

    upload to work on other machine

---
+++ restaurant-page/src/About/Gallery.js
@@ -0,0 +1,5 @@
+const Gallery = (props) => {
+    
+}
+
+export default Gallery;
--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,13 +1,14 @@
 import './styles.css'
 import Home from './Home';
 import Menu from './Menu';
+import About from './About';
 import NavBarLogic from './NavBar/logic';
 
 const navBarLogic = new NavBarLogic();
 
 const content = document.getElementById('content');
 // const home = Home({ parent: content });
-
-const about = Menu({ parent: content });
+const menu = Menu({ parent: content });
+// const about = About({ parent: content });
 
 console.log("working");
