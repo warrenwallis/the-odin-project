88f5a546793d1a8064c5e8677fa104f1d1e23e25
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-02-04 15:42:38 -0800

    Refactor Code for Component Use
    Refactor Star to also include the button and logic for switching between stars
    Refactor SearchBar to be a component that can be used in multiple places

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,3 +1,4 @@
+import List from './List';
 import Menu from './Menu';
 import './styles.css'
 
@@ -6,4 +7,5 @@ console.log('working');
 const App = (() => {
     const body = document.body;
     const menu = Menu({ parent: body });
+    const list = List({ parent: body, title: 'Personal' });
 })();
