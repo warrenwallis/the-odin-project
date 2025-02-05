8670afa295563505c9be42fd759286c635ec1d4d
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-02-04 17:57:43 -0800

    Style List Content
    Render tasks in a list.
    Refactor Tab to be more general and handle different use cases.
    Add images for checking whether or not a task is finished.

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,11 +1,14 @@
 import List from './List';
 import Menu from './Menu';
 import './styles.css'
+import information from './assets/information.json'
 
 console.log('working');
 
 const App = (() => {
+    const { defaultLists } = information;
+
     const body = document.body;
     const menu = Menu({ parent: body });
-    const list = List({ parent: body, title: 'Personal' });
+    const list = List({ parent: body, list: defaultLists[0] });
 })();
