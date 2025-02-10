92daeda66a48621be88696fbc1cfc45691798233
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-02-10 15:32:58 -0800

    Be Able to Add Lists
    New lists can now be added.
    Adding new tasks and lists will remain after page refreshes and browser closes since it now uses local storage

--- todo-list/src/Menu/AllLists.js
+++ todo-list/src/Menu/AllLists.js
@@ -1,11 +1,11 @@
 import information from '../assets/information.json';
 import Tab from '../components/Tab';
-import { getListIdx, setListIdx } from '../services/storage';
+import { getAllLists, getListIdx, setListIdx } from '../services/storage';
 
 const AllLists = (props) => {
-    const { parent, setShowListIdx, render, searchText } = props;
+    const { parent, render, searchText } = props;
     const showListIdx = getListIdx();
-    const { defaultLists } = information;
+    const lists = getAllLists();
     const container = document.createElement('div');
 
     const constructor = (() => {
@@ -14,7 +14,7 @@ const AllLists = (props) => {
         container.setAttribute('style', 'display: flex; flex-direction: column; gap: 10px');
 
         container.textContent = 'All Lists';
-        for (const [ idx, { title }] of defaultLists.entries()) {
+        for (const [ idx, { title }] of lists.entries()) {
             if (searchText === '') {
                 Tab({ parent: container, title: title, addDescription: false, star: true, starred: true, styles: `height: 60px; ${idx == showListIdx ? 'background: var(--dark-moss-green)' : ''}`, showListIdx, idx, setListIdx, render });
             } else if (title.toLowerCase().includes(searchText)) {
