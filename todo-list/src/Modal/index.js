f7d0d012bf3d451fe1380744a042e66ceed62ef2
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-02-11 17:22:57 -0800

    Style Modal

---
+++ todo-list/src/Modal/index.js
@@ -0,0 +1,65 @@
+import Star from "../components/Star";
+import calendar from '../assets/calendar.png';
+import noteSrc from '../assets/note.png';
+
+const Modal = (props) => {
+    const { parent } = props;
+
+    const constructor = (() => {
+        const shadow = document.createElement('div');
+        const container = document.createElement('div'); // change to form when styled
+        const header = document.createElement('div');
+        const title = document.createElement('input')
+        const description = document.createElement('textarea');
+        const dateDiv = document.createElement('div');
+        const dateImg = document.createElement('img');
+        const date = document.createElement('input');
+        const noteDiv = document.createElement('div');
+        const noteHeader = document.createElement('div');
+        const noteImg = document.createElement('img');
+        const noteTitle = document.createElement('span');
+        const note = document.createElement('textarea');
+        const buttonsSpacer = document.createElement('div');
+        const buttonsDiv = document.createElement('div');
+        const exitButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
+        header.append(title);
+        const star = Star({ parent: header, styles: 'width: 30px' });
+        dateDiv.append(dateImg, date);
+        noteHeader.append(noteImg, noteTitle);
+        noteDiv.append(noteHeader, note);
+        buttonsDiv.append(exitButton, deleteButton);
+        buttonsSpacer.append(buttonsDiv);
+        container.append(header, description, dateDiv, noteDiv, buttonsSpacer);
+        parent.append(shadow, container);
+
+        shadow.setAttribute('style', 'width: 70%; height: 100%; background-color: var(--tigers-eye); opacity: 70%');
+        container.setAttribute('style', 'display: flex; flex-direction: column; width: 30%; background-color: var(--tigers-eye); padding: 50px 20px; gap: 25px')
+        header.setAttribute('style', 'display: flex; justify-content: space-between');
+        title.setAttribute('style', 'border: 2px solid black; background: var(--tigers-eye); font-weight: var(--semibold); font-size: 32px; color: var(--cornsilk)');
+        description.setAttribute('style', 'border: 2px solid black; background: var(--tigers-eye); font-size: 16px; color: var(--cornsilk); height: 10%');
+        dateDiv.setAttribute('style', 'display: flex; align-items: center; gap: 10px');
+        dateImg.setAttribute('style', 'width: 25px');
+        date.setAttribute('style', 'border: 2px solid black; background: var(--tigers-eye); font-size: 16px; color: var(--cornsilk); font-size: 20px');
+        noteDiv.setAttribute('style', 'display: flex; flex-direction: column; gap: 10px; height: 45%');
+        noteHeader.setAttribute('style', 'display: flex; align-items: center; color: var(--cornsilk); font-size: 20px; gap: 10px');
+        noteImg.setAttribute('style', 'width: 25px');
+        note.setAttribute('style', 'border: 2px solid black; background: var(--tigers-eye); font-size: 16px; color: var(--cornsilk); font-size: 20px; height: 100%');
+        buttonsSpacer.setAttribute('style', 'display: flex; align-items: end; width: 100%; height: 100%');
+        buttonsDiv.setAttribute('style', 'display: flex; gap: 25px; width: 100%');
+        exitButton.setAttribute('style', 'width: 50%; height: 30px; border-radius: 5px; background: var(--earth-yellow); border: 0;  color: var(--cornsilk)');
+        deleteButton.setAttribute('style', 'width: 50%; height: 30px; border-radius: 5px; background: var(--earth-yellow); border: 0;  color: var(--cornsilk)');
+
+        title.placeholder = 'title';
+        description.placeholder = 'description';
+        dateImg.src = calendar;
+        date.placeholder = 'date';
+        noteImg.src = noteSrc;
+        noteTitle.textContent = 'Note';
+        note.placeholder = 'Notes'
+        exitButton.textContent = 'Exit';
+        deleteButton.textContent = 'Delete';
+    })();
+}
+
+export default Modal;
--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,18 +3,22 @@ import Menu from './Menu';
 import './styles.css'
 import information from './assets/information.json'
 import * as storageService from './services/storage';
+import Modal from './Modal';
 
 console.log('working');
 
 const App = (() => {
     const { defaultLists } = information;
     let [ showListIdx, setShowListIdx ] = [ 0, (listIdx) => showListIdx = listIdx ];
-    const body = document.body;
-
+    let [ showModal, setShowModal ] = [ false, (m) => showModal = m ];
+    const content = document.getElementById('content');
+    const modalDiv = document.getElementById('modal');
+    
     const render = () => {
-        body.innerHTML = '';
-        const menu = Menu({ parent: body, setShowListIdx, render });
-        const list = List({ parent: body });
+        content.innerHTML = '';
+        const modal = Modal({ parent: modalDiv });
+        const menu = Menu({ parent: content, setShowListIdx, render });
+        const list = List({ parent: content });
     }
 
     const constructor = (() => {
