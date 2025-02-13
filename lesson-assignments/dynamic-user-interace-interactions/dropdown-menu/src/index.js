f9c47c5032bff369ca6ee32eb7ad87637075bb3a
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-02-13 14:56:17 -0800

    Finish Dropdown Assignment

--- lesson-assignments/dynamic-user-interace-interactions/dropdown-menu/src/index.js
+++ lesson-assignments/dynamic-user-interace-interactions/dropdown-menu/src/index.js
@@ -1,4 +1,18 @@
-import './styles.css'
-import { greeting } from './greeting';
+import "./styles.css";
 
-console.log(greeting);
+const dropdownMenu = document.getElementById("dropdown-menu");
+const dropdownButton = document.getElementById("dropdown-button");
+dropdownButton.addEventListener("click", () => {
+	if (dropdownMenu.style.display === "") {
+		dropdownMenu.style.display = "flex";
+	} else {
+		dropdownMenu.style.display = "";
+	}
+});
+
+const menuButtons = document.getElementsByClassName("menu-buttons");
+Array.from(menuButtons).forEach((element) => {
+	element.addEventListener("click", () => {
+		window.alert(`Clicked ${element.textContent}`);
+	});
+});
