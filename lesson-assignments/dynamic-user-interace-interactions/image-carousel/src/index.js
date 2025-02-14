cec719afdfeb7c6ecb84dbcf2190f22a303e2f1f
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-02-14 14:45:26 -0800

    Finish Carousel

--- lesson-assignments/dynamic-user-interace-interactions/image-carousel/src/index.js
+++ lesson-assignments/dynamic-user-interace-interactions/image-carousel/src/index.js
@@ -1,4 +1,42 @@
-import './styles.css'
-import { greeting } from './greeting';
+import "./styles.css";
 
-console.log(greeting);
+const slides = document.getElementById("slides");
+const slideWidth = document.querySelector(".slide").clientWidth; // Get width of one image
+const indexButtons = document.getElementById("index-buttons");
+let index = 0;
+
+updateIndexButton();
+
+document.getElementById("next").addEventListener("click", () => {
+	if (index < slides.children.length - 1) {
+		index++;
+	} else {
+		index = 0; // Loop back to first image
+	}
+	updateCarousel();
+	updateIndexButton();
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+	if (index > 0) {
+		index--;
+	} else {
+		index = slides.children.length - 1; // Loop back to last image
+	}
+	updateCarousel();
+	updateIndexButton();
+});
+
+function updateCarousel() {
+	slides.style.transform = `translateX(${-index * slideWidth}px)`;
+}
+
+function updateIndexButton() {
+	Array.from(indexButtons.children).forEach((element, idx) => {
+		if (idx === index) {
+			element.style.background = "var(--primary-color)";
+		} else {
+			element.style.background = "white";
+		}
+	});
+}
