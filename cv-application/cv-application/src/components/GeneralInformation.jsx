7e280dd1f3093f4ee7a5bb75239b93fdd1898c49
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2025-10-05 14:56:20 -0700

    finish UI of General Information

--- cv-application/cv-application/src/components/GeneralInformation.jsx
+++ cv-application/cv-application/src/components/GeneralInformation.jsx
@@ -1,21 +1,33 @@
+import logo from "../assets/generalInformation.png"
+
 function GeneralInformation() {
 	return (
 		<div className="flex flex-col items-center py-10 border border-red-500 text-gray-900">
-			<div className="w-full text-2xl font-medium mb-15">
+			<div className="w-full text-2xl font-medium mb-10">
 				General Information
 			</div>
-			<div className="">
-				<Input title="First Name" />
+			<div className="flex w-full">
+				<div className="flex flex-col w-3/5">
+					<div className="flex justify-between">
+						<Input title={"First Name"} />
+						<Input title={"Last Name"} />
+					</div>
+					<Input title={"Email"} size={"full"}/>
+					<Input title={"Phone Number"} />
+				</div>
+				<div className="flex justify-center flex-1">
+					<img src={logo} />
+				</div>
 			</div>
 		</div>
 	);
 }
 
-function Input({ title = null }) {
+function Input({ title = null, size = "47/100" }) {
 	return (
-		<div className="flex flex-col text-lg font-medium">
+		<div className={`flex flex-col my-2 text-lg font-medium w-${size}`}>
 			{title}
-			<input className="p-2 mt-1 rounded-md bg-gray-700 text-gray-100 focus:outline-none border-3 border-transparent focus:border-gray-400" />
+			<input className="p-2 mt-1 rounded-xl bg-gray-900 text-gray-100 focus:outline-none border-3 border-transparent focus:border-gray-400" />
 		</div>
 	);
 }
