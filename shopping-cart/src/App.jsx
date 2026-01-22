0a26f9ab54310cc990ff63f2de6f458f21d15a32
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2026-01-21 16:07:50 -0800

    add button functionality to navigation bar

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
+	const [pageIdx, setPageIdx] = useState(0);
 	return (
 		<>
-			<NavigationBar />
+			<NavigationBar pageIdx={pageIdx} setPageIdx={setPageIdx} />
 			<div className="w-full h-full px-10 py-20 flex justify-center">
 				<Home />
 			</div>
--- shopping-cart/src/components/NavigationBar.jsx
+++ shopping-cart/src/components/NavigationBar.jsx
@@ -1,44 +1,45 @@
-import { useState } from "react";
-
-const pages = { 0: "Home", 1: "Shop", 2: "Cart" };
-
-function NavigationBar() {
-	const [pageIdx, setPageIdx] = useState(0);
+function NavigationBar({ pageIdx, setPageIdx }) {
 	return (
 		<div className="bg-white px-10 py-5 shadow-xl flex justify-between">
 			<div className="flex-1">
-				<button className="font-bold flex items-center text-2xl">
+				<button
+					className="font-bold flex items-center text-2xl"
+					onClick={() => setPageIdx(0)}
+				>
 					<i className="fa-solid fa-shop icon-primary text-5xl mr-2"></i>
 					ShopHub
 				</button>
 			</div>
 			<div className="flex-1 flex justify-end text-xl text-primary">
 				<button
-					className={`mx-4 px-4 py-2 rounded-lg ${
+					className={`mx-4 px-4 py-2 rounded-lg  ${
 						pageIdx === 0
 							? "bg-(--color-primary)/20 text-(--color-primary)"
-							: ""
+							: "hover:bg-gray-100"
 					}`}
+					onClick={() => setPageIdx(0)}
 				>
 					<i className="fa-solid fa-house mr-2"></i>
 					Home
 				</button>
 				<button
-					className={`mx-4 px-4 py-2 rounded-lg ${
+					className={`mx-4 px-4 py-2 rounded-lg  ${
 						pageIdx === 1
 							? "bg-(--color-primary)/20 text-(--color-primary)"
-							: ""
+							: "hover:bg-gray-100"
 					}`}
+					onClick={() => setPageIdx(1)}
 				>
 					<i className="fa-solid fa-shop mr-2"></i>
 					Shop
 				</button>
 				<button
-					className={`mx-4 px-4 py-2 rounded-lg ${
+					className={`mx-4 px-4 py-2 rounded-lg  ${
 						pageIdx === 2
 							? "bg-(--color-primary)/20 text-(--color-primary)"
-							: ""
+							: "hover:bg-gray-100"
 					}`}
+					onClick={() => setPageIdx(2)}
 				>
 					<i className="fa-solid fa-cart-shopping mr-2"></i>
 					Cart
