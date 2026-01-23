b0e39d6db79238c9f1ee240c27d600edb77f3ef9
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2026-01-22 20:25:54 -0800

    add link functionality to call to action

--- shopping-cart/src/components/CallToAction.jsx
+++ shopping-cart/src/components/CallToAction.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function CallToAction() {
 	return (
 		<div className="w-full max-w-6xl flex flex-col items-center gap-6 mt-10 mb-10 bg-(--color-primary) rounded-lg p-8 shadow-lg">
@@ -7,10 +9,13 @@ function CallToAction() {
 			<p className="text-gray-200 text-xl">
 				Explore our products and build your perfect cart today
 			</p>
-			<button className="bg-white hover:bg-gray-200 text-(--color-primary) font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300">
+			<Link
+				to="/shop"
+				className="bg-white hover:bg-gray-200 text-(--color-primary) font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300"
+			>
 				Go to Shop
 				<i className="fa-solid fa-arrow-right ml-2"></i>
-			</button>
+			</Link>
 		</div>
 	);
 }
